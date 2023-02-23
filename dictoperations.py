f92258535ed6a79f2206462c4233bca1cedba318
Abhishek <idiot63666@gmail.com>
2023-02-23 09:56:40 +0530

    Create dictoperations.py

---
+++ dictoperations.py
@@ -0,0 +1,22 @@
+# Create a dictionary
+my_dict = {'apple': 1, 'banana': 2, 'cherry': 3}
+
+# Get all items in the dictionary
+items = my_dict.items()
+print(items)
+
+# Get all values in the dictionary
+values = my_dict.values()
+print(values)
+
+# Create a copy of the dictionary
+my_dict_copy = my_dict.copy()
+print(my_dict_copy)
+
+# Get all keys in the dictionary
+keys = my_dict.keys()
+print(keys)
+
+# Update the dictionary with new key-value pairs
+my_dict.update({'orange': 4, 'pear': 5})
+print(my_dict)
